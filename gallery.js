0d9f25f0ba475a3e57bce2ce85d511268b8d2559
Maya <mayahilbert@gmail.com>
2020-02-14 08:46:25 -0500

    add htaccess and remove html from urls

--- gallery.js
+++ gallery.js
@@ -1,36 +1,36 @@
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+var slideIndex = [1,1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+var dotIndex = [1,1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var dotId = ["dots1", "dots2", "dots3"]
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+// Thumbnail image controls
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var slides = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName(dotId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < x.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  x[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " active";
 }
